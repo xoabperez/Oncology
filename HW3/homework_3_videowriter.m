f7418063833da4a11b4df54332bd3ccff07fd47f
Xoab Perez <35434658+xoabperez@users.noreply.github.com>
2018-03-07 14:03:56 -0600

    homework 3 progress

--- HW3/homework_3_videowriter.m
+++ HW3/homework_3_videowriter.m
@@ -162,55 +162,55 @@ xlim([0 250]);% ylim([0 18000]);
 saveas(gcf,'2b2','jpg');
     
 %% Problem 2c
-clear; clc; close all
-dx = 0.1; 
-dy =0.1;
-sx = 10/dx; 
-sy = 10/dy;
-dt = 0.005;
-carcap = 1; % carrying capacity
-D = 0.005*ones(sy,sx); % mm^2/day
-
-N_o = zeros(sy,sx); % aerobic cells
-N_g = zeros(sy,sx); % glycolysis cells
-N_o(40:60,40:60) = 0.75*carcap; % initialize N_o
-L = zeros(sy,sx);  % initialize lactate
-G = 2*ones(sy,sx);  % initialize glucose
-chi_L = 0*G;
-chi_G = 0*G;
-Dno = D; Dng = D;
-Dg = 5*D; Dl = D; 
-
-% Initialize model parameters
-kg  = 5;  % N_g growth rate
-ko  = 1;  % N_o growth rate
-kog = 15; % conversion from N_o to N_g 
-kgo = 3;  % conversion from N_g to N_o
-gamma = 75; % sensitivity switch parameter
-Gmin  = 0.2;
-Lstar = .25; % Lactate threshold
-Gstar = 0.5; 
-Mstar = 0.1;
-Nstar = 1;
-alpha_G = 1;
-alpha_L = 10;
-beta_G =10;%1.15;
-beta_L = 3.2;
-beta_o = 1.6;
-
-%Plotting parameters
-No = zeros(1,5/dt+1); Ng = No; Gtot = No; Ltot = No;
-No(1) = sum(sum(N_o)); Ng(1) = sum(sum(N_g));
-G_init = sum(sum(G));
-Gtot(1) = G_init-sum(sum(G)); Ltot(1) = sum(sum(L));
-fig = figure('position',[600 500 600 500]);
+% clear; clc; close all
+% dx = 0.1; 
+% dy =0.1;
+% sx = 10/dx; 
+% sy = 10/dy;
+% dt = 0.005;
+% carcap = 1; % carrying capacity
+% D = 0.005*ones(sy,sx); % mm^2/day
+% 
+% N_o = zeros(sy,sx); % aerobic cells
+% N_g = zeros(sy,sx); % glycolysis cells
+% N_o(40:60,40:60) = 0.75*carcap; % initialize N_o
+% L = zeros(sy,sx);  % initialize lactate
+% G = 2*ones(sy,sx);  % initialize glucose
+% chi_L = 0*G;
+% chi_G = 0*G;
+% Dno = D; Dng = D;
+% Dg = 5*D; Dl = D; 
+% 
+% % Initialize model parameters
+% kg  = 5;  % N_g growth rate
+% ko  = 1;  % N_o growth rate
+% kog = 15; % conversion from N_o to N_g 
+% kgo = 3;  % conversion from N_g to N_o
+% gamma = 75; % sensitivity switch parameter
+% Gmin  = 0.2;
+% Lstar = .25; % Lactate threshold
+% Gstar = 0.5; 
+% Mstar = 0.1;
+% Nstar = 1;
+% alpha_G = 1;
+% alpha_L = 10;
+% beta_G =10;%1.15;
+% beta_L = 3.2;
+% beta_o = 1.6;
+% 
+% %Plotting parameters
+% No = zeros(1,5/dt+1); Ng = No; Gtot = No; Ltot = No;
+% No(1) = sum(sum(N_o)); Ng(1) = sum(sum(N_g));
+% G_init = sum(sum(G));
+% Gtot(1) = G_init-sum(sum(G)); Ltot(1) = sum(sum(L));
+fig = figure('position',[100 100 1600 900]);
 set(fig,'Visible','off');
-writerObj = VideoWriter('flat.avi','Motion JPEG AVI');
-writerObj.Quality = 75;
+writerObj = VideoWriter('newvile10.avi','Motion JPEG AVI');
+writerObj.Quality = 100;
 writerObj.FrameRate = 10;
 open(writerObj); 
 
-for t = 2:(1/dt)
+for t = 901:(5/dt)
     chi_G(G>Gmin) = 1; chi_G(G<Gmin) = 0; chi_G(G==Gmin) = 0;
     chi_L = .5*(1+tanh(gamma*(L-Lstar)));
     No_new = rd_fdm(N_o,Dno,[dx,dy],dt)+dt*(ko.*N_o.*(1-(N_o+N_g)/carcap)+...
@@ -227,30 +227,75 @@ for t = 2:(1/dt)
     No(t) = sum(sum(N_o)); Ng(t) = sum(sum(N_g));
     Gtot(t) = G_init-sum(sum(G)); Ltot(t) = sum(sum(L));
     
-    subplot(2,2,1)
-    imagesc(N_o(:,:));
-    %title('Concentration of Oxidative Cells');
-    
+%     subplot(2,3,1)
+%     imagesc(N_o(:,:));
+%     title('Concentration of Oxidative Cells');
+%     xlabel('x position');
+%     ylabel('y position');
+%     colorbar
+% 
+%     subplot(2,3,2)
+%     imagesc(N_g(:,:));
+%     title('Concentration of Glycolitic Cells');
+%     xlabel('x position');
+%     ylabel('y position');
+%     colorbar
 
-    subplot(2,2,3)
-    imagesc(N_g(:,:));
-    %title('Concentration of Glycolitic Cells');
+    subplot(2,3,1)
+    mesh(N_o(:,:));
+    zlim([0 1]);
+    title('Concentration of Oxidative Cells');
+    xlabel('x position');
+    ylabel('y position');
+    zlabel('Normalized amount of cells');
     
+    subplot(2,3,2)
+    mesh(G(:,:));
+    zlim([0 2]);
+    title('Glucose Concentration');
+    xlabel('x position');
+    ylabel('y position');
+    zlabel('Normalized amount');
     
-    subplot(2,2,2)
-    imagesc(G(:,:));
-    %title('Glucose Concentration');
+    subplot(2,3,4)
+    mesh(N_g(:,:));
+    zlim([0 1]);
+    title('Concentration of Glycolitic Cells');
+    xlabel('x position');
+    ylabel('y position');
+    zlabel('Normalized amount of cells');
     
+    subplot(2,3,5)
+    mesh(L(:,:));
+    zlim([0 1]);
+    title('Lactate Concentration');
+    xlabel('x position');
+    ylabel('y position');
+    zlabel('Normalized amount');
+
+    subplot(2,3,3)
+    plot(1:t, No(1:t), 1:t, Ng(1:t));
+    xlabel('Iteration');
+    ylabel('Number of Cells');
+    title('Oxidative vs Glycolitic Cells');
+    legend('Oxidative','Glycolitic');
+    xlim([0 1000]); ylim([0 5000]);
+
+    subplot(2,3,6)
+    plot(1:t, Gtot(1:t), 1:t, Ltot(1:t));
+    xlabel('Iteration');
+    ylabel('Amount');
+    title('Glucose Consumption and Lactate Level');
+    legend('Glucose Consumed','Lactate');
+    xlim([0 1000]); ylim([0 5000]);
     
-    subplot(2,2,4)
-    imagesc(L(:,:));
-    %title('Lactate Concentration');
-    if mod(t,3)==0
+    if mod(t,5)==0
         F = getframe(fig);
         writeVideo(writerObj,F);
     end
 end
 close(fig)
+close(writerObj)
 
 %% Problem 3a (1D optimization)
 clear;
